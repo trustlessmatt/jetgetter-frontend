0d50717600f8d81e82750634da554cde281a6540
Matt Pfeifer <matthewcpfeifer@gmail.com>
2022-05-12 00:42:29 -0400

    roadmap skele with text

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Home from "./components/Home";
+import Roadmap from "./components/Roadmap";
 
 function App() {
   return (
     <div>
       <Home />
+      <Roadmap />
     </div>
   );
 }
--- src/components/Roadmap.js
+++ src/components/Roadmap.js
@@ -1,12 +1,146 @@
-
-
 const Roadmap = () => {
   return (
     <div>
-      Testing
+      {/* outer container for all roadmap items */}
+      <div>
+        {/* block 1 */}
+        <div className="shadow-lounge-mid-purple/50 shadow-md ml-4 my-4 border-2 border-lounge-dark-purple rounded-xl w-1/2">
+          <div className="p-3">
+            <h1>Introduction 🛬</h1>
+            <p>
+              Once in a lifetime experiences should be for everyone. For the
+              average person, traveling to another state can already be a tall
+              task let alone another country or around the world. As the tourism
+              industry bounces back and grows, we believe it should be
+              accessible to everyone. Others may want to keep it as a fantasy,
+              keep it as something only the "wealthy" can do or the "retired".
+              Fuck that, we’re here to see everyone experience the world.
+              <br />
+              With over a decade facilitating experiences around the world &
+              generations in the travel industry, we are pioneering travel in
+              Web3. While others just look to make a profit & the rest want to
+              keep it out of reach, we're a group of passionate travelers that
+              just want to share with other passionate travelers. Our philosophy
+              is simple: Traveling will change your life.
+              <br />
+              This is NOT an investment opportunity, moon play, passive income,
+              or nothing like that. Think of it as replacing travel credit card
+              perks & shit. Your NFT is an exclusive access pass to a growing
+              brand & community in this space, one that will reward its holders,
+              provide value to the ecosystem, and have everyone enjoying life
+              while we do it.
+            </p>
+          </div>
+        </div>
+        {/* block 2 */}
+        <div className="shadow-lounge-mid-purple/50 shadow-md ml-auto mr-4 my-4 border-2 border-lounge-dark-purple rounded-xl w-1/2">
+          <div className="p-3">
+            <h1>30 Seconds in an Elevator ⏱</h1>
+            <p>
+              “What’s JetGetter Club?” A Brand utilizing Web3 to make travel
+              accessible & build the dopest travel community in the world.
+              “How?” Accepting crypto for curated travel bookings & creating a
+              platform to provide resources for the best experiences in the
+              world (talk about it).
+            </p>
+          </div>
+        </div>
+        {/* block 3 */}
+        <div className="shadow-lounge-mid-purple/50 shadow-md ml-4 my-4 border-2 border-lounge-dark-purple rounded-xl w-1/2">
+          <div className="p-3">
+            <h1>Why JetGetter? 🛩</h1>
+            <p>
+              We welcome all PFPs & Normies.
+              <br />
+              JetGetter Club was founded to build the best travel community in
+              Web3 & our NFTs will give you access to some of the best
+              experiences in the world. Bond with others over your passion to
+              see the world and be put on to things you never knew of before.
+              This is not some cookie cutter shit either, the way we see travel
+              is about exploring cultures & places to the fullest not just
+              tourist attractions (Though those are still fun). When our Mint
+              Sells out, we will be sending 20 people on a trip to one of our
+              partnered destinations.
+              <br />
+              There is one thing we know forsure, more people need to travel and
+              more people need to be invested into crypto. Our goal is to help
+              onboard in both ways while we catch vibes together. Life is about
+              balance and doing the things that you love, even if you don’t
+              travel if you believe in that ethos, you’re in the right place.
+            </p>
+          </div>
+        </div>
+        {/* block 4 */}
+        <div className="shadow-lounge-mid-purple/50 shadow-md ml-auto mr-4 my-4 border-2 border-lounge-dark-purple rounded-xl w-1/2">
+          <div className="p-3">
+            <h1>What does it mean to be a JetGetter? 🤔</h1>
+            <p>
+              It’s a lifestyle, rep your PFP online & do JetGetter shit IRL.
+              <br />
+              Simply put, you’re a dope individual that knows how to enjoy life.
+              You’re a tastemaker. You’re open minded. You want to experience
+              the most you can in this life. You like good food. You like to do
+              activities. You like to learn about others cultures. & you use all
+              these things to shape your perspective in life. Lastly, you
+              understand the balance between wealth and enjoying the fruits of
+              your labor. & if you’re none of these things but want to learn,
+              we’re here to welcome you.
+            </p>
+          </div>
+        </div>
+        {/* block 5 */}
+        <div className="shadow-lounge-mid-purple/50 shadow-md ml-4 my-4 border-2 border-lounge-dark-purple rounded-xl w-1/2">
+          <div className="p-3">
+            <h1>What are we building? 🧱</h1>
+            <p>
+              <ol>
+                <li>
+                  1. Community: Our community will be the “Travel Influencer”
+                  but the most authentic one you’ve come across. People coming
+                  together to share their experiences with others, learn from
+                  one another, form bonds that may lead to taking trips
+                  together, meetups when you’re in each others cities, and just
+                  an overall place to have fun around our similar interests.
+                </li>
+                <li>
+                  2. Media: How do we showcase our community as the tastemakers
+                  of Web3? We build & use our platform to showcase our expertise
+                  by providing quality travel content that is authentic &
+                  informative. We’ll be looking for video editors & quality
+                  content created to be featured on various social media pages.
+                  This is an opportunity for us to create partnerships, reward &
+                  showcase members of our community, bring some realness to the
+                  travel industry and grow the overall brand. Aside from
+                  creating content, there has been a rising desire from our
+                  community to put all the info we share in discord in a
+                  consolidated way. Post mint we’ll explore building a social
+                  media like platform to have all the information easily
+                  accessible.
+                </li>
+                <li>
+                  3.Travel: This is the alpha you’ve been waiting for. We are
+                  suppliers of full vacation packages in a number of countries
+                  (India, Dubai, Greece, Maldives, Bhutan) to name them. What
+                  that mean is we already have partnerships with airlines,
+                  hotels, drivers, and guides where we have curated itineraries
+                  that are customizable based on your interests/request. As far
+                  as the airlines, it is currently only for those flying out of
+                  the US but anyone international can still take advantage of
+                  everything else in any of those destinations. These trips are
+                  also perfect for NFT communities to connect IRL as we can
+                  service up to a 500 person trip.
+                </li>
+                <li>
+                  4. A brand: A mixture of Michelin Guide & Louis Vuitton with
+                  more culture.
+                </li>
+              </ol>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
-
-}
+};
 
 export default Roadmap;
