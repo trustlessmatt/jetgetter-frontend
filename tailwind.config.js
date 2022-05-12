3ca289e85fcca95fceef8b2f0969ac44f16007cd
Matt Pfeifer <matthewcpfeifer@gmail.com>
2022-05-12 00:09:28 -0400

    styling assets updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lounge: {
+          'light-purple': '#a8sdec',
+          'mid-purple' : '#6c25bc',
+          'dark-purple' : '#4f1b88',
+          'floor-purple' : '#7e02e6',
+          'white-couch' : '#e8e7df',
+          'gold' : '#c5942d',
+          'dark-panel' : '#160435',
+          'blueprint' : '#1622dd'
+        }
+      }
+    },
   },
   plugins: [],
 }
