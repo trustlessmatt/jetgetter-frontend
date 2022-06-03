a53897159085c9a6d0ceb3a2a378cdef6e34faa7
matt.sol <matthewcpfeifer@gmail.com>
2022-06-03 11:25:20 -0400

    navigation now with other content on mobile

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import lounge_gif from "../assets/lounge.gif";
 import lounge_mp4 from "../assets/Lounge.mp4";
 import Header from "../Header";
@@ -9,33 +11,53 @@ const Home = () => {
   return (
     <div>
       <div className="relative">
-        <Header />
-          <video
-            src={lounge_mp4}
-            muted
-            autoPlay={"autoplay"}
-            preload="auto"
-            className="w-full hidden sm:block absolute z-10"
+      <div className="hidden sm:block"><Header /></div>
+        <video
+          src={lounge_mp4}
+          muted
+          autoPlay={"autoplay"}
+          preload="auto"
+          className="w-full hidden sm:block absolute z-10"
+          // onClick={() => {
+          //   transcendRef.current.scrollIntoView({ behavior: "smooth" });
+          // }}
+          loop
+        >
+          <source
+            alt="clouds"
+            type="video/mp4"
             // onClick={() => {
             //   transcendRef.current.scrollIntoView({ behavior: "smooth" });
             // }}
-            loop
-          >
-            <source
-              alt="clouds"
-              type="video/mp4"
-              // onClick={() => {
-              //   transcendRef.current.scrollIntoView({ behavior: "smooth" });
-              // }}
-            />
-          </video>
-          <img src={lounge_gif} alt="lounge" className="sm:hidden absolute z-0" />
+          />
+        </video>
+        <img src={lounge_gif} alt="lounge" className="sm:hidden absolute z-0" />
         <div className="absolute sm:top-20 top-[300px] left-0 right-0 z-30 mx-auto">
           <h1 className="text-lounge-gold text-center lg:text-[100px] md:text-[80px] sm:text-[60px] text-[70px]">
             JetGetter Club
           </h1>
         </div>
-        <div className="absolute z-30 top-[420px] left-0 right-0 sm:hidden">
+        <div className="absolute z-30 top-[400px] left-0 right-0 sm:hidden">
+          <div className="flex justify-center items-center py-2 space-x-8 sm:ml-auto sm:mr-1 mx-auto">
+            {/* Home */}
+            <div>
+              <Link to="/">
+                <p className="text-lounge-gold text-3xl font-jose">Home</p>
+              </Link>
+            </div>
+            {/* Blueprint */}
+            <div>
+              <Link to="/blueprint">
+                <p className="text-lounge-gold text-3xl font-jose">Blueprint</p>
+              </Link>
+            </div>
+            {/* Shop */}
+            <div className="hover:cursor-not-allowed">
+              <p className="text-lounge-gold/30 text-3xl font-jose">Shop</p>
+            </div>
+          </div>
+        </div>
+        <div className="absolute z-30 top-[450px] left-0 right-0 sm:hidden">
           <div className="flex items-center justify-center">
             {/* twitter */}
             <div className="rounded-lg mx-2">
