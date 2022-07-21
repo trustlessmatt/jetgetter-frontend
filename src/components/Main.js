2f66d24cb302624eb20b4221a8c741af78411271
matt.sol <matthewcpfeifer@gmail.com>
2022-07-21 14:00:44 -0400

    bug fix naming convention

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import Home from './pages/home';
+import Home from './pages/Home';
 import About from './pages/about';
 
 const Main = () => {
